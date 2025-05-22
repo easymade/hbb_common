c27b258187ee99e1cb0f8e8531c5cc0249d0d82b
yaozhiguang <ayaonihao@163.com>
2025-05-22 23:08:56 +0800

    修改私有RENDEZVOUS_SERVERS和RS_PUB_KEY

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,10 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+// pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
+// pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["218.60.8.206"];
+pub const RS_PUB_KEY: &str = "TpP0rxc5x9AIqmdCjYYRg8gal8y4xiJYTekZmwOBoZA=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
